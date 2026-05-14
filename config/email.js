6e7926d043557d1bbcd255e32354a6132cab8598
emex <emex@emex.emex>
2026-05-14 11:23:09 +0100

    .env updated

--- config/email.js
+++ config/email.js
@@ -3,14 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER || "simplyemem10@gmail.com",
+    pass: process.env.EMAIL_PASS || "wsfouyrkrsqljtie"
   }
 });
 
 exports.sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
-    from: `"Nick Maurice Nursing Academy" <${process.env.EMAIL_USER}>`,
+    from: `Nick Maurice Nursing Academy`,
     to,
     subject,
     html
